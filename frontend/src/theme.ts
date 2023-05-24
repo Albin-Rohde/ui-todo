cd6b33f0e37fcf3143609edf86b5f58e9a0c5133
Albin Rohde <albin.rohde@kundo.se>
2023-05-24 23:57:12 +0200

    Add Material ui for styling and theme
    Instead of a large css file, we'll use material ui's themeing feature to theme and style the app in a cohesive manner

---
+++ frontend/src/theme.ts
@@ -0,0 +1,17 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
+
+export default theme;
