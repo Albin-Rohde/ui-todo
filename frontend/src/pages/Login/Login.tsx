9eede29f91886107311537f5db087b4135203ef6
Albin Rohde <albin.rohde@kundo.se>
2023-05-25 22:37:03 +0200

    Add Login component
    This component is the root of the Login page.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Box} from '@mui/material';
+import LoginForm from './LoginForm';
+import AppName from "../../components/AppName";
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+  },
+  heading: {
+    marginBottom: '1rem',
+    textAlign: 'center',
+  },
+};
 
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <Box sx={styles.container}>
+      <Box>
+        <AppName animate styles={{marginBottom: '5vh', textAlign: 'center'}}/>
+        <LoginForm/>
+      </Box>
+    </Box>
+  );
 }
 
 export default Login;
