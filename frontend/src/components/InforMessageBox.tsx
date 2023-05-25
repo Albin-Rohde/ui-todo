a051e048d0811bbb19ee959835a08acb22572823
Albin Rohde <albin.rohde@kundo.se>
2023-05-25 22:35:05 +0200

    Add InfoMessageBox component
    This component displays a info message inside a lightgray box

---
+++ frontend/src/components/InforMessageBox.tsx
@@ -0,0 +1,27 @@
+import {styled} from "@mui/system";
+import {Box, ButtonProps} from "@mui/material";
+import React from "react";
+
+
+const CustomBox = styled(Box)(({theme}) => ({
+  backgroundColor: '#f5f5f5',
+  padding: theme.spacing(2),
+  borderRadius: theme.shape.borderRadius,
+  marginTop: theme.spacing(2),
+  width: 320,
+  maxWidht: '90vw'
+}));
+
+interface PrimaryButtonProps extends ButtonProps {
+  children: React.ReactNode;
+}
+
+const InforMessageBox: React.FC<PrimaryButtonProps> = ({children}) => {
+  return (
+    <CustomBox>
+      {children}
+    </CustomBox>
+  );
+};
+
+export default InforMessageBox;
