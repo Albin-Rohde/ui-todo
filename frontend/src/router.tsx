94e0d6ce0a634f8560963b433df167345d076b66
Albin Rohde <albin.rohde@kundo.se>
2023-05-26 20:52:29 +0200

    Add test for router component
    This means slightly altering the router component to make it easier to test

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,26 +1,30 @@
-import * as React from "react";
-import {createBrowserRouter} from "react-router-dom";
+import * as React from 'react';
+import { useRoutes } from 'react-router-dom';
 
-import Dashboard from "./pages/Dashboard/Dashboard";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
+import Dashboard from './pages/Dashboard/Dashboard';
+import LandingPage from './pages/LandingPage/LandingPage';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
-export const router = createBrowserRouter([
+export const routes = [
   {
-    path: "/",
+    path: '/',
     element: <LandingPage/>,
   },
   {
-    path: "/dashboard",
+    path: '/dashboard',
     element: <Dashboard/>,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login/>,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: <Signup/>,
   }
-]);
+];
+
+export const Router = (): React.ReactElement | null => {
+  return useRoutes(routes);
+}
