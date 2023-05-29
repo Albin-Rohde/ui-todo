92c0d0f2f2bed9b6a5192b80d62686eaed4ab1c4
Albin Rohde <albin.rohde@kundo.se>
2023-05-29 19:12:24 +0200

    Implement signin functionality to frontend

--- frontend/src/pages/SignIn/SignInForm.tsx
+++ frontend/src/pages/SignIn/SignInForm.tsx
@@ -1,10 +1,12 @@
 import { Box, Link as MuiLink, TextField, Typography } from '@mui/material';
 import { styled } from '@mui/system';
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
 import InforMessageBox from '../../components/InforMessageBox';
 import PrimaryButton from '../../components/PrimaryButton';
+import useHttp from '../../hooks/useHttp';
+import { isValidationError } from '../../typeguard';
 
 const Container = styled(Box)(() => ({
   display: 'flex',
@@ -14,7 +16,67 @@ const Container = styled(Box)(() => ({
   mt: 2,
 }));
 
+interface FormFieldError {
+  email: string,
+  password: string,
+}
+
 function SignInForm() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [formError, setFormError] = useState<FormFieldError>({
+    email: '',
+    password: '',
+  });
+  const { loading, sendRequest } = useHttp();
+  const navigate = useNavigate();
+
+  const checkFormForError = (): { ok: boolean, errors: FormFieldError } => {
+    let ok = true;
+    const errors: FormFieldError = {
+      email: email ? '' : 'Email is required.',
+      password: password ? '' : 'Password is required.',
+    }
+    if (!email || !password) {
+      ok = false;
+    }
+    return { ok, errors };
+  }
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setFormError({
+      email: '',
+      password: '',
+    });
+    const { ok, errors } = checkFormForError();
+    if (!ok) {
+      setFormError(errors);
+      return;
+    }
+
+    const response = await sendRequest({
+      path: '/user/signin',
+      method: 'POST',
+      body: {
+        email,
+        password,
+      }
+    })
+
+    if (!response.ok && isValidationError(response.err)) {
+      const { field, message } = response.err.extra
+      setFormError({
+        ...errors,
+        [field]: message,
+      });
+      return;
+    }
+    if (response.data === 'ok') {
+      navigate('/');
+    }
+  }
+
   return (
     <Container>
       <Box component="form" sx={{
@@ -24,9 +86,26 @@ function SignInForm() {
         gap: 2,
         maxWidth: 350
       }}>
-        <TextField label="Email" type="email" variant="outlined" fullWidth/>
-        <TextField label="Password" type="password" variant="outlined" fullWidth/>
-        <PrimaryButton variant="contained" color="primary" fullWidth>
+        <TextField
+          label="Email"
+          type="email"
+          variant="outlined"
+          fullWidth
+          error={formError.email !== ''}
+          helperText={formError.email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          variant="outlined"
+          fullWidth
+          error={formError.password !== ''}
+          helperText={formError.password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <PrimaryButton variant="contained" color="primary" fullWidth
+                       onClick={handleSubmit}>
           Sign in
         </PrimaryButton>
       </Box>
