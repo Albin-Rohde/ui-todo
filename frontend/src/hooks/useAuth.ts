5eb9965a0b95afb856470e159440b8dc08bbbd11
Albin Rohde <albin.rohde@kundo.se>
2023-05-30 12:07:44 +0200

    Add UserContext to store authenticated user
    Also make the useAuth hook store the user in context
    and other small fixes to the auth hook
    
    With this hook, we can display the real username in UserProfile component

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { UserContext } from '../contexts/UserContext';
+import { User } from '../types';
 
 import useHttp from './useHttp';
 
@@ -13,27 +16,42 @@ export const useAuth = (redirect: RedirectOptions) => {
     redirect.redirectOn = 'loggedOut';
   }
 
-  const { sendRequest } = useHttp();
+  const { user, setUser } = useContext(UserContext);
+  const { sendRequest, error } = useHttp();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (user) {
+      setLoading(false)
+      redirect && redirect.redirectOn === 'loggedIn' && navigate(redirect.redirectTo);
+      return;
+    }
     const checkAuth = async () => {
-      const response = await sendRequest({ path: '/user/session', method: 'GET' });
+      const { ok, data } = await sendRequest<User>({
+        path: '/user/session',
+        method: 'GET'
+      });
 
-      if (response.ok && response.data === 'ok') {
+      if (ok && data) {
         // User is logged in, allow access
+        setUser(data);
         setLoading(false);
-        redirect && redirect.redirectOn === 'loggedOut' && navigate(redirect.redirectTo);
+        redirect && redirect.redirectOn === 'loggedIn' && navigate(redirect.redirectTo);
       } else {
         // User is not logged in, redirect to sign-in page
         setLoading(false);
-        redirect && redirect.redirectOn === 'loggedIn' && navigate(redirect.redirectTo);
+        redirect && redirect.redirectOn === 'loggedOut' && navigate(redirect.redirectTo);
       }
     };
-
     checkAuth();
-  }, [navigate, sendRequest]);
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      navigate('/signin');
+    }
+  }, [error]);
 
   return { loading };
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+import { UserContextProvider } from './contexts/UserContext';
 import reportWebVitals from './reportWebVitals';
 import { Router } from './router';
 import theme from './theme';
@@ -15,9 +16,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Router/>
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Router/>
+        </BrowserRouter>
+      </UserContextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
