0cdb0a07f52431e7ae0d64e3899d0d863e0fed37
Albin Rohde <albin.rohde@kundo.se>
2023-05-31 22:32:33 +0200

    Add TodoItemService with tests

---
+++ backend/src/todoitem/service.ts
@@ -0,0 +1,91 @@
+import { User } from "../user/entity/User";
+import { EntityNotFoundError, Repository } from "typeorm";
+import { db } from "../data-source";
+import { TodoItem } from "./entity/TodoItem";
+import { TodoList } from "../todolist/entity/TodoList";
+import { TodoListService } from "../todolist/service";
+
+type CreateTodoItemInput = {
+  user: User;
+  publicListId: TodoList["publicId"];
+  text: TodoItem["text"];
+  completed?: TodoItem["completed"];
+}
+
+type UpdateTodoItemInput = {
+  user: User
+  id: TodoItem["id"];
+  publicListId: TodoList["publicId"];
+  text?: TodoItem["text"];
+  completed?: TodoItem["completed"];
+}
+
+type DeleteTodoItemInput = {
+  user: User;
+  id: TodoItem["id"];
+  publicListId: TodoList["publicId"];
+}
+
+export class TodoItemService {
+  constructor(
+    private readonly todoItemRepository: Repository<TodoItem> = db.getRepository(TodoItem),
+    private readonly todoListService: TodoListService = new TodoListService(),
+  ) {
+  }
+
+  public async create(input: CreateTodoItemInput) {
+    const list = await this.todoListService.getByPublicId(input.user, input.publicListId)
+    if (!list) {
+      throw new EntityNotFoundError(TodoList, "TodoList not found");
+    }
+    const item = new TodoItem();
+    item.text = input.text;
+    item.completed = input.completed || false;
+    item.list = list;
+    return this.todoItemRepository.save(item);
+  }
+
+  public async update(input: UpdateTodoItemInput) {
+    const list = await this.todoListService.getByPublicId(input.user, input.publicListId)
+    if (!list) {
+      throw new EntityNotFoundError(TodoList, "TodoList not found");
+    }
+    const item = await this.todoItemRepository.findOneOrFail({ where: { id: input.id } });
+    if (input.text !== undefined) {
+      item.text = input.text;
+    }
+    if (input.completed !== undefined) {
+      item.completed = input.completed;
+    }
+    return this.todoItemRepository.save(item);
+  }
+
+  public async delete(input: DeleteTodoItemInput) {
+    const { user, id, publicListId } = input;
+    const list = await this.todoListService.getByPublicId(user, publicListId)
+    const item = await this.todoItemRepository.findOneOrFail({
+      where: {
+        id,
+        listId: list.id
+      }
+    });
+    return this.todoItemRepository.remove(item);
+  }
+
+  public async getByListId(user: User, listId: string) {
+    const list = await this.todoListService.getByPublicId(user, listId)
+    if (!list) {
+      throw new EntityNotFoundError(TodoList, "TodoList not found");
+    }
+    return this.todoItemRepository.find({ where: { listId: list.id }, order: { id: "ASC" } });
+  }
+
+  public responseFormat(todoItem: TodoItem) {
+    return {
+      id: todoItem.id,
+      text: todoItem.text,
+      complete: todoItem.completed,
+      listId: todoItem.listId,
+    };
+  }
+}
---
+++ backend/src/todoitem/test/service.test.ts
@@ -0,0 +1,101 @@
+import { db } from "../../data-source";
+import { TodoItemService } from "../service";
+import {
+  TodoItemFactory,
+  TodoListFactory,
+  UserFactory
+} from "../../test-utils/factories";
+import { faker } from "@faker-js/faker";
+import { EntityNotFoundError } from "typeorm";
+
+describe("TodoItemService", () => {
+  let todoItemService: TodoItemService;
+
+  beforeAll(async () => {
+    todoItemService = new TodoItemService();
+    await db.initialize();
+  });
+
+  afterEach(async () => {
+    await db.synchronize(true);
+  });
+
+  afterAll(async () => {
+    await db.close();
+  });
+
+  describe("create", () => {
+    it("should create a todoitem linked to list", async () => {
+      const user = await new UserFactory().create();
+      const todoList = await new TodoListFactory().create({ user });
+      const todoItem = await todoItemService.create({
+        user,
+        publicListId: todoList.publicId,
+        text: faker.lorem.text(),
+        completed: false,
+      });
+      expect(todoItem).toBeDefined();
+      expect(todoItem.id).toBeDefined();
+      expect(todoItem.list.id).toEqual(todoList.id);
+    });
+
+    it("should not create a todoitem linked to list if list does not exist", async () => {
+      const user = await new UserFactory().create();
+      await expect(
+        todoItemService.create({
+          user,
+          publicListId: faker.string.uuid(),
+          text: faker.lorem.text(),
+          completed: false,
+        })
+      ).rejects.toThrowError(EntityNotFoundError);
+    });
+
+    it("should not create a todoitem linked to list if user does not own list", async () => {
+      const user = await new UserFactory().create();
+      const user2 = await new UserFactory().create();
+      const todoList = await new TodoListFactory().create({ user: user2 });
+
+      await expect(
+        todoItemService.create({
+          user,
+          publicListId: todoList.publicId,
+          text: faker.lorem.text(),
+          completed: false,
+        })
+      ).rejects.toThrowError(EntityNotFoundError);
+
+      const count = await db.createQueryBuilder()
+        .select("todo_item")
+        .from("todo_item", "todo_item")
+        .getCount();
+      expect(count).toEqual(0);
+    });
+  });
+
+  describe("update", () => {
+    it("should update a todoitem", async () => {
+      const user = await new UserFactory().create();
+      const todoList = await new TodoListFactory().create({ user });
+      const todoItem = await new TodoItemFactory().create({ list: todoList });
+
+      const newText = faker.lorem.text();
+      await todoItemService.update({
+        user,
+        id: todoItem.id,
+        publicListId: todoList.publicId,
+        text: newText,
+        completed: true,
+      });
+
+      const item = await db.createQueryBuilder()
+        .select("todo_item")
+        .from("todo_item", "todo_item")
+        .getOne();
+      expect(item).toBeDefined();
+      expect(item?.id).toEqual(todoItem.id);
+      expect(item?.text).toEqual(newText);
+      expect(item?.completed).toEqual(true);
+    });
+  });
+});
