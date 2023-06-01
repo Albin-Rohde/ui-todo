c232098fd164c2f1a83b5261559f0636e7dafbfd
Albin Rohde <albin.rohde@kundo.se>
2023-06-01 17:36:48 +0200

    Add ListOfRecentLists Component
    This component will fetch and display all 'recent lists', Ie lists from other user that this user has visited.

---
+++ frontend/src/components/ListOfRecentLists.tsx
@@ -0,0 +1,65 @@
+import { Box, CircularProgress, List, ListItem, ListItemText, } from '@mui/material';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { RecentListsContext } from '../contexts/RecentListsContext';
+import useHttp from '../hooks/useHttp';
+
+const ListOfRecentLists = () => {
+  const { loading: loadingFetchList, sendRequest: sendFetchList } = useHttp();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { recentLists, setRecentlists } = useContext(RecentListsContext);
+
+  const fetchTodoLists = async () => {
+    const response = await sendFetchList<[{
+      publicId: string;
+      id: number;
+      name: string;
+    }]>({
+      path: '/todo-list/recent',
+      method: 'GET',
+    });
+
+    if (response.ok && response.data) {
+      setRecentlists(response.data)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodoLists();
+  }, []);
+
+  const listItems = () => {
+    if (loadingFetchList) {
+      return (
+        <CircularProgress/>
+      )
+    }
+
+    return recentLists.map((list) => {
+      const isSelected = list.publicId === id;
+      return (
+        <ListItem
+          key={list.id}
+          button
+          onClick={() => navigate(`/list/${list.publicId}`)}
+          selected={isSelected}
+        >
+          <ListItemText primary={list.name}/>
+        </ListItem>
+      )
+    });
+  }
+
+
+  return (
+    <>
+      <Box marginLeft={1} marginRight={1} width="calc(100% - 1rem)">
+      </Box>
+      <List>{listItems()}</List>
+    </>
+  );
+};
+
+export default ListOfRecentLists;
