426f829da7e0cb27e5590f63d1fa727c63cd5eef
Albin Rohde <albin.rohde@kundo.se>
2023-06-02 18:00:18 +0200

    Change design of todoitems list

--- frontend/src/pages/Dashboard/TodoItems.tsx
+++ frontend/src/pages/Dashboard/TodoItems.tsx
@@ -1,6 +1,6 @@
 import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import RemoveCircleOutlineOutlinedIcon
+  from '@mui/icons-material/RemoveCircleOutlineOutlined';
 import {
   Box,
   Divider,
@@ -8,26 +8,21 @@ import {
   List,
   ListItem,
   ListItemIcon,
-  ListItemText,
-  MenuItem,
-  MenuList,
-  Paper,
-  Popover,
-  Typography,
+  Tooltip,
 } from '@mui/material';
-import React, { MouseEvent, useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
+import SecondaryButton from '../../components/SecondaryButton';
 import TodoItemCheckbox from '../../components/TodoItemCheckbox';
 import { TodoItemContext } from '../../contexts/TodoItemsContext';
 import useHttp from '../../hooks/useHttp';
+import { TodoItem } from '../../types';
 
 const TodoItems = () => {
   const { todoItems, setTodoItems } = useContext(TodoItemContext);
   const { sendRequest } = useHttp();
   const { id } = useParams<{ id: string }>();
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
 
   const handleAddItemClick = async () => {
     const response = await sendRequest<{
@@ -45,104 +40,68 @@ const TodoItems = () => {
     setTodoItems([...todoItems, response.data]);
   };
 
-  const handleDeleteItemClick = async () => {
-    if (!selectedItemId) {
-      console.log('no selected item')
-      return;
-    }
+  const handleDeleteItemClick = async (todoItem: TodoItem) => {
+    setTodoItems((prev: TodoItem[]) => {
+      return prev.filter((item) => item.id !== todoItem.id);
+    });
     await sendRequest({
-      path: `/todo-list/${id}/todo-item/${selectedItemId}`,
+      path: `/todo-list/${id}/todo-item/${todoItem.id}`,
       method: 'DELETE',
     });
-    setTodoItems(todoItems.filter((item) => item.id !== selectedItemId));
-    handleClosePopover();
   }
-  const handleMoreIconClick = (event: MouseEvent<HTMLButtonElement>, itemId: number) => {
-    setAnchorEl(event.currentTarget);
-    setSelectedItemId(itemId);
-  };
-
-  const handleClosePopover = () => {
-    setAnchorEl(null);
-    setSelectedItemId(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const popoverId = open ? 'popover-menu' : undefined;
 
   const getRowBackgroundColor = (index: number) => {
+    return '#f8f8f8'
     return index % 2 === 0 ? '#e4ebf3' : '#cbddf1';
   }
   const todoItemsList = todoItems?.map((item, index) => {
     return (
-      <ListItem sx={{
-        paddingLeft: '0',
-        width: '100%',
-        backgroundColor: getRowBackgroundColor(index)
-      }} key={item.id}>
-        <TodoItemCheckbox
-          item={item}
-          fontSize="1rem"
-        />
-        <ListItemIcon sx={{ margin: 0, padding: 0, minWidth: '24px' }}>
-          <IconButton sx={{ margin: 0, padding: 0 }}
-                      onClick={(e) => handleMoreIconClick(e, item.id)}>
-            <MoreVertOutlinedIcon/>
-          </IconButton>
-        </ListItemIcon>
-      </ListItem>
+      <>
+        <Divider/>
+        <ListItem sx={{
+          paddingLeft: '0',
+          width: '100%',
+          backgroundColor: getRowBackgroundColor(index)
+        }} key={item.id}>
+          <TodoItemCheckbox
+            item={item}
+            fontSize="1rem"
+          />
+          <ListItemIcon sx={{ margin: 0, padding: 0, minWidth: '24px' }}>
+            <Tooltip title={'Delete item'}>
+              <IconButton sx={{ padding: 0 }} onClick={() => handleDeleteItemClick(item)}>
+                <RemoveCircleOutlineOutlinedIcon
+                  sx={{ fontSize: '1rem', color: '#c02828' }}/>
+              </IconButton>
+            </Tooltip>
+          </ListItemIcon>
+        </ListItem>
+      </>
     );
   });
 
   return (
     <Box sx={{ display: 'flex', textAlign: 'left', width: '100%' }}>
       <List sx={{ width: '100%', paddingTop: 0 }}>
-        <ListItem sx={{ paddingLeft: '0', width: '100%', backgroundColor: '#cbddf1' }}
-                  key="add-item">
-          <ListItemIcon sx={{
-            margin: 0,
-            padding: 0,
-            paddingLeft: 1,
-            paddingY: 0.5,
-            minWidth: '24px',
-          }}>
-            <IconButton sx={{ margin: 0, padding: 0 }} onClick={handleAddItemClick}>
-              <AddIcon/>
-            </IconButton>
-            <Typography sx={{ marginLeft: 2, fontStyle: 'italic' }}>
-              Add item
-            </Typography>
-          </ListItemIcon>
+        <ListItem
+          sx={{
+            paddingLeft: '.5rem',
+            width: '100%',
+            backgroundColor: '#f8f8f8',
+            height: '3rem'
+          }}
+          key="add-item"
+        >
+          <SecondaryButton
+            sx={{ backgroundColor: '#f4f4f4', height: '2.2rem' }}
+            onClick={handleAddItemClick}
+          >
+            <AddIcon/> Add item
+          </SecondaryButton>
         </ListItem>
         {todoItemsList}
         <Divider/>
       </List>
-
-      <Popover
-        id={popoverId}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClosePopover}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-      >
-        <Paper sx={{ width: 150, maxWidth: '100%' }}>
-          <MenuList>
-            <MenuItem onClick={handleDeleteItemClick}>
-              <ListItemIcon>
-                <DeleteIcon fontSize="small"/>
-              </ListItemIcon>
-              <ListItemText>Delete</ListItemText>
-            </MenuItem>
-          </MenuList>
-        </Paper>
-      </Popover>
     </Box>
   );
 };
