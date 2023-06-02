7070ce387ac2edbecf2989da29ef9f1e5e49c83d
Albin Rohde <albin.rohde@kundo.se>
2023-06-02 18:00:17 +0200

    Add functionality to set list as private

--- frontend/src/components/TodoListOptions.tsx
+++ frontend/src/components/TodoListOptions.tsx
@@ -1,5 +1,7 @@
 import LockIcon from '@mui/icons-material/Lock';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import { IconButton, ListItemIcon, Tooltip } from '@mui/material';
 import React from 'react';
 
@@ -11,31 +13,68 @@ interface TodoListOptionsProps {
 }
 
 export const TodoListOptions = (props: TodoListOptionsProps) => {
-  const readonlyHelpText = 'Readonly, only you can edit this list';
-  const publicHelpText = 'Public, anyone with the link can edit this list';
+  const activeColor = '#007afe';
+  const inActiveColor = '#979797';
+
   const { todoList, updateList } = props;
 
   return (
-    <ListItemIcon>
-      {todoList.readonly ? (
-        <Tooltip title={readonlyHelpText} arrow>
-          <IconButton onClick={(e) => {
-            e.stopPropagation();
-            updateList({ ...todoList, readonly: false })
-          }}>
-            <LockIcon style={{ fontSize: '1.2rem' }}/>
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title={publicHelpText} arrow>
-          <IconButton onClick={(e) => {
-            e.stopPropagation();
-            updateList({ ...todoList, readonly: true })
-          }}>
-            <LockOpenIcon style={{ fontSize: '1.2rem' }}/>
-          </IconButton>
-        </Tooltip>
-      )}
-    </ListItemIcon>
+    <>
+      <ListItemIcon sx={{ minWidth: '0' }}>
+        {todoList.private ? (
+          <Tooltip title={'Only you can see this list'}>
+            <IconButton
+              onClick={(e) => {
+                e.stopPropagation();
+                updateList({ ...todoList, private: false })
+              }}
+              sx={{ padding: 0, paddingTop: '4px', paddingRight: '0.2rem' }}
+            >
+              <VisibilityOffIcon style={{ fontSize: '1.4rem', color: activeColor }}/>
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title={'Anyone with link can see this list'}>
+            <IconButton
+              onClick={(e) => {
+                e.stopPropagation();
+                updateList({ ...todoList, private: true })
+              }}
+              sx={{ padding: 0, paddingTop: '4px', paddingRight: '0.2rem' }}
+            >
+              <VisibilityOutlinedIcon
+                style={{ fontSize: '1.4rem', color: inActiveColor }}/>
+            </IconButton>
+          </Tooltip>
+        )}
+      </ListItemIcon>
+      <ListItemIcon sx={{ minWidth: '0' }}>
+        {todoList.readonly ? (
+          <Tooltip title={'Only you can edit this list'} arrow>
+            <IconButton
+              onClick={(e) => {
+                e.stopPropagation();
+                updateList({ ...todoList, readonly: false })
+              }}
+              sx={{ padding: 0 }}
+            >
+              <LockIcon style={{ fontSize: '1.5rem', color: activeColor }}/>
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title={'Anyone with the link can edit this list'} arrow>
+            <IconButton
+              onClick={(e) => {
+                e.stopPropagation();
+                updateList({ ...todoList, readonly: true })
+              }}
+              sx={{ padding: 0 }}
+            >
+              <LockOpenIcon style={{ fontSize: '1.5rem', color: inActiveColor }}/>
+            </IconButton>
+          </Tooltip>
+        )}
+      </ListItemIcon>
+    </>
   );
 }
