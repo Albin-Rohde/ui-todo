b813e2eee49ad52a650c0cce0b4aa1baac15acbd
Albin Rohde <albin.rohde@kundo.se>
2023-06-03 12:31:06 +0200

    Wrap fakeCursor with tooltip to display who is typing

--- frontend/src/components/FakeCursor.tsx
+++ frontend/src/components/FakeCursor.tsx
@@ -1,25 +1,31 @@
+import { Tooltip } from '@mui/material';
 import React from 'react';
 
 interface FakeCursorProps {
   top?: number;
   left: number;
+  username: string;
 }
 
-export const FakeCursor = ({ top, left }: FakeCursorProps) => {
+export const FakeCursor = ({ top, left, username }: FakeCursorProps) => {
   top = top || 0 + 18;
   left = left + 41;
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top,
-        left,
-        width: '2px',
-        height: '1.2em',
-        backgroundColor: '#007afe',
-      }}
-      className="fake-cursor">
-      ‎
-    </div>
+    <Tooltip
+      title={`${username} is typing`} arrow open={true}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top,
+          left,
+          width: '2px',
+          height: '1.2em',
+          backgroundColor: '#007afe',
+        }}
+        className="fake-cursor">
+        ‎
+      </div>
+    </Tooltip>
   );
 };
