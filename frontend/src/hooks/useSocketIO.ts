8854df6d0277cff66159dd340da9e975c6798e4f
Albin Rohde <albin.rohde@kundo.se>
2023-06-03 22:16:55 +0200

    Move Join/Leave logic into the listView component
    It did not make sense for the UseSocketIO hook to be responsible for handling joining/leaving room
    
    This makes for a more clear separation of concern

--- frontend/src/hooks/useSocketIO.ts
+++ frontend/src/hooks/useSocketIO.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 import { SocketContext } from '../contexts/SocketContext';
@@ -10,7 +10,6 @@ const useSocketIO = () => {
   const { socket, setSocket, isConnected, setIsConnected } = useContext(SocketContext);
   const { todoList, setTodolist } = useContext(TodoListContext)
   const { todoItems, setTodoItems } = useContext(TodoItemContext);
-  const [prevId, setPrevId] = useState<string | null>(null);
 
   useEffect(() => {
     if (socket) {
@@ -34,18 +33,9 @@ const useSocketIO = () => {
   }, []);
 
   useEffect(() => {
-    if (todoList && socket && isConnected) {
-      if (prevId !== null && prevId !== todoList.publicId) {
-        socket.emit('todolist.leave-room', { id: prevId });
-      }
-      socket.emit('todolist.join-room', { id: todoList.publicId });
-      setPrevId(todoList.publicId);
-    }
-  }, [todoList]);
-
-  useEffect(() => {
     if (socket && !isConnected) {
       socket.connect();
+
       socket.on('todolist.list-updated', (data: TodoList) => {
         if (todoList && data.id === todoList.id) {
           setTodolist(data);
@@ -75,6 +65,8 @@ const useSocketIO = () => {
       })
     }
   }, [socket]);
+
+  return { socket, isConnected };
 };
 
 
