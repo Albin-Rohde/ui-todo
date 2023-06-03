6e4f6f914d19025c3e8b4ebc1eb01db851f56011
Albin Rohde <albin.rohde@kundo.se>
2023-06-04 00:19:21 +0200

    UseSocketIO updates cursor position state on events from backend
    We want to put all our event listeners inside of this hook, to get a clear understanding on when client state is updated due to websocket events.

--- frontend/src/hooks/useSocketIO.ts
+++ frontend/src/hooks/useSocketIO.ts
@@ -4,12 +4,14 @@ import { io } from 'socket.io-client';
 import { SocketContext } from '../contexts/SocketContext';
 import { TodoItemContext } from '../contexts/TodoItemsContext';
 import { TodoListContext } from '../contexts/TodoListContext';
-import { TodoItem, TodoList } from '../types';
+import { UserContext } from '../contexts/UserContext';
+import { CursorPosition, TodoItem, TodoList } from '../types';
 
 const useSocketIO = () => {
   const { socket, setSocket, isConnected, setIsConnected } = useContext(SocketContext);
   const { todoList, setTodolist } = useContext(TodoListContext)
-  const { todoItems, setTodoItems } = useContext(TodoItemContext);
+  const { todoItems, setTodoItems, setCursorPositions } = useContext(TodoItemContext);
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     if (socket) {
@@ -58,6 +60,20 @@ const useSocketIO = () => {
           const newItems = [...todoItems, data];
           setTodoItems(newItems);
         }
+      });
+
+      socket?.on('todoitem.cursor-pos-updated', (data: CursorPosition) => {
+        setCursorPositions((prev) => {
+          // remove old cursor position
+          const filtered = prev.filter((cursorPoss) => {
+            return cursorPoss.userId !== user?.id;
+          });
+          // add new cursor position
+          return [
+            ...filtered,
+            data,
+          ];
+        });
       });
 
       socket.on('connect', () => {
