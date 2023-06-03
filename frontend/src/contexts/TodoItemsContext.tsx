1f498569739f8d1e88031ed23a899395458c19d8
Albin Rohde <albin.rohde@kundo.se>
2023-06-03 22:35:19 +0200

    Add cursorPositions state to TodoItemsContext
    It makes sense to put the cursorPositions state inside this contextprovider, since the cursor state is very closely related to the items themselves.

--- frontend/src/contexts/TodoItemsContext.tsx
+++ frontend/src/contexts/TodoItemsContext.tsx
@@ -1,13 +1,17 @@
 import React, { createContext, useState } from 'react';
 
-import { TodoItem } from '../types';
+import { CursorPosition, TodoItem } from '../types';
 
 const TodoItemContext = createContext<{
   todoItems: TodoItem[];
+  cursorPositions: CursorPosition[];
   setTodoItems: React.Dispatch<React.SetStateAction<TodoItem[]>>
+  setCursorPositions: React.Dispatch<React.SetStateAction<CursorPosition[]>>
 }>({
   todoItems: [],
+  cursorPositions: [],
   setTodoItems: () => null,
+  setCursorPositions: () => null,
 });
 
 interface UserContextProps {
@@ -16,9 +20,11 @@ interface UserContextProps {
 
 const TodoItemContextProvider: React.FC<UserContextProps> = ({ children }) => {
   const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+  const [cursorPositions, setCursorPositions] = useState<CursorPosition[]>([]);
 
   return (
-    <TodoItemContext.Provider value={{ todoItems, setTodoItems }}>
+    <TodoItemContext.Provider
+      value={{ todoItems, setTodoItems, cursorPositions, setCursorPositions }}>
       {children}
     </TodoItemContext.Provider>
   );
