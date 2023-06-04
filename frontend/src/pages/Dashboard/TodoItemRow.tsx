89696dfc26c47359bc72628d074c18dc8c487657
Albin Rohde <albin.rohde@kundo.se>
2023-06-04 13:03:20 +0200

    TodoItemRow is responsible delete
    Since this component has everything it needs to execute delete of item, we do not need to send it in as props. We can make this component handle it directly.

--- frontend/src/pages/Dashboard/TodoItemRow.tsx
+++ frontend/src/pages/Dashboard/TodoItemRow.tsx
@@ -2,19 +2,35 @@ import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import RemoveCircleOutlineOutlinedIcon
   from '@mui/icons-material/RemoveCircleOutlineOutlined';
 import { IconButton, ListItem, ListItemIcon, Tooltip } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 
 import TodoItemCheckbox from '../../components/TodoItemCheckbox';
+import { TodoItemContext } from '../../contexts/TodoItemsContext';
+import useHttp from '../../hooks/useHttp';
 import { TodoItem } from '../../types';
 
 interface TodoItemRowProps {
   item: TodoItem;
-  handleAddItemClick: (parentId?: number) => void;
-  handleDeleteItemClick: (todoItem: TodoItem) => void;
+  handleAddItemClick: (item: TodoItem) => void;
   paddingLeft?: string;
 }
 
 export const TodoItemRow = (props: TodoItemRowProps) => {
+  const { id } = useParams<{ id: string }>();
+  const { sendRequest } = useHttp();
+  const { setTodoItems } = useContext(TodoItemContext);
+
+  const handleDeleteItemClick = async (todoItem: TodoItem) => {
+    setTodoItems((prev: TodoItem[]) => {
+      return prev.filter((item) => item.id !== todoItem.id);
+    });
+    await sendRequest({
+      path: `/todo-list/${id}/todo-item/${todoItem.id}`,
+      method: 'DELETE',
+    });
+  }
+
   return (
     <ListItem
       sx={{
@@ -31,18 +47,23 @@ export const TodoItemRow = (props: TodoItemRowProps) => {
       <ListItemIcon
         sx={{ margin: 0, padding: 0, paddingRight: '4px', minWidth: '24px' }}>
         <Tooltip title={'Add sub-item'}>
-          <IconButton sx={{ padding: 0 }}
-                      onClick={() => props.handleAddItemClick(props.item.id)}>
+          <IconButton
+            sx={{ padding: 0 }}
+            onClick={() => props.handleAddItemClick(props.item)}
+          >
             <PlaylistAddIcon sx={{ fontSize: '1.5rem' }}/>
           </IconButton>
         </Tooltip>
       </ListItemIcon>
       <ListItemIcon sx={{ margin: 0, padding: 0, minWidth: '24px' }}>
         <Tooltip title={'Delete item'}>
-          <IconButton sx={{ padding: 0 }}
-                      onClick={() => props.handleDeleteItemClick(props.item)}>
+          <IconButton
+            sx={{ padding: 0 }}
+            onClick={() => handleDeleteItemClick(props.item)}
+          >
             <RemoveCircleOutlineOutlinedIcon
-              sx={{ fontSize: '1.5rem', color: '#c02828' }}/>
+              sx={{ fontSize: '1.5rem', color: '#c02828' }}
+            />
           </IconButton>
         </Tooltip>
       </ListItemIcon>
