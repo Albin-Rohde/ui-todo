d8f1085dd02799de5d988b8964db7280ca4c5fb5
Albin Rohde <albin.rohde@kundo.se>
2023-06-05 23:51:05 +0200

    Add Dashboard Component
    This Component should be used to wrap everything that is viewed at from the dashboard. Giving the child components the sidepanel etc

---
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,30 @@
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+
+import SidePanel from '../../components/SidePanel';
+
+import { TopBar } from './components/TopBar';
+
+interface DashboardProps {
+  children: React.ReactNode;
+}
+
+export const Dashboard = (props: DashboardProps) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  }
+
+  return (
+    <>
+      {isMobile && <TopBar toggleDrawer={toggleDrawer}/>}
+      <Box display={'flex'}>
+        <SidePanel toggleDrawer={toggleDrawer} open={drawerOpen} isMobile={isMobile}/>
+        {props.children}
+      </Box>
+    </>
+  )
+}
